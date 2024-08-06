e0a40493b98b93e86a3b43aeed69156ba5612870
Ganesh <ch.tejamr@gmail.com>
2024-08-07 02:49:42 +0530

    connected with the deployed backend server

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({})
-  const backendURL = 'http://localhost:5000'
+  const backendURL = 'https://food-delivery-backend-66v4.onrender.com'
   const [token, setToken] = useState("")
   const [food_list, setFoodList] = useState([])
 
